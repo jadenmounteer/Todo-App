45132cef978c8d88a0cbf149805bfc1a17cd34da
jmounteer <mounteerjaden@gmail.com>
2021-10-11 07:27:43 -0600

    Added HTML markup and Shia Labeouf.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    // jQuery methods go here...
+    
+    $(document).mouseenter(function(){
+        console.log("Playing the video");
+        document.getElementById("shia-labouf").play();
+      });
+        
+});
