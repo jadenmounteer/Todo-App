680633e1097cb6a90793cf5be203a43b59e1a8dc
jmounteer <mounteerjaden@gmail.com>
2021-10-11 08:06:27 -0600

    Set delay on video appearing

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
     // jQuery methods go here...
     
     $(document).mouseenter(function(){
-        console.log("Playing the video");
-        document.getElementById("shia-labouf").play();
+      // Play the video
+      document.getElementById("shia-labouf").play();
+      // Show the video
+      setTimeout(function(){ document.getElementById("shia-labouf").style.display = "block"; }, 3000);
+      
       });
         
 });
