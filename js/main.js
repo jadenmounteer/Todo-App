8c3aedec9498d3a90c8c374ab78f1603c8dd0b5a
jmounteer <mounteerjaden@gmail.com>
2021-10-13 08:46:35 -0600

    Added tap event for mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     
     // jQuery methods go here...
-  
+
+    // PC
     $(document).mouseenter(function(){
+      // Play the video
+      document.getElementById("shia-labouf").play();
+      // Show the video
+      document.getElementById("shia-labouf").style.display = "block";
+      //setTimeout(function(){ document.getElementById("shia-labouf").style.display = "block"; }, 3000);
+      
+      });
+
+    // mobile
+    $(document).on("tap", function(){
       // Play the video
       document.getElementById("shia-labouf").play();
       // Show the video
