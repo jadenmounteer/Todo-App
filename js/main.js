c148cf3534cf5fdc6d82ff78eed3ed30edbad46f
jmounteer <mounteerjaden@gmail.com>
2021-10-14 18:00:46 -0600

    Finished styles. Added animations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
     
     // jQuery methods go here...
 
+    // Make the title type itself in
+    /*
+    var i = 0;
+    var txt = 'Just Do It';
+    var speed = 150;
+
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("page-title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+
+    typeWriter();
+    */
+
+    // Make Shia appear 
+
     // PC
     $(document).mouseenter(function(){
       // Play the video
@@ -21,6 +40,8 @@ $(document).ready(function(){
       //setTimeout(function(){ document.getElementById("shia-labouf").style.display = "block"; }, 3000);
       
       });
+
+  
       
         
 });
