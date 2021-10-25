15a2bf5654a5f1c993993b59069746592a6a90c1
jmounteer <mounteerjaden@gmail.com>
2021-10-25 08:45:18 -0600

    update iphone support

--- js/main.js
+++ js/main.js
@@ -4,46 +4,14 @@ import  TodoList from './todos.js';
 // Check if the device is an iphone
 var isIphone = navigator.userAgent.indexOf("iPhone") != -1 ;
 
-/*
-// JQuery 
-$(document).ready(function(){
-    
-    // jQuery methods go here...
-
-    // Make Shia appear 
-  
-    // PC
-    $(document).mouseenter(function(){
-      // Play the video
-      //document.getElementById("shia-labouf").play();
-      // Show the video
-      //document.getElementById("shia-labouf").style.display = "block";
-      //document.getElementById("shia-labouf").muted = false;
-     
-      
-      });
-
-    // mobile
-    $(document).on("tap", function(){
-      // Play the video
-      //document.getElementById("shia-labouf").play();
-      //document.getElementById("shia-labouf").muted = false;
-  
-      
-      });
-
-        
-});
-*/
-
 // Create an instance of Todos
 let todoList = new TodoList("todoListKey");
 
 // Initiate the todoList
 todoList.initiate();
 
-// If the device is not an iphone, remove the controls and umute the video
-if (!isIphone) {
-  document.getElementById("shia-labouf").controls = false;
-  document.getElementById("shia-labouf").muted = false;
+// If the device is an iphone, remove autoplay and add the controls so the user can play the video
+if (isIphone) {
+  document.getElementById("shia-labouf").autoplay = false;
+  document.getElementById("shia-labouf").controls = true;
 }
